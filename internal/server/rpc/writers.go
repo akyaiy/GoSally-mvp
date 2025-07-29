86cdc9adf21e410cb47763ce93be59110fc1753c
Alexey <alex@oblat.lv>
2025-07-29 11:27:36 +0300

    remove header and status

--- internal/server/rpc/writers.go
+++ internal/server/rpc/writers.go
@@ -2,32 +2,22 @@ package rpc
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
-func write(w http.ResponseWriter, status int, msg any) error {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-
-	switch m := msg.(type) {
-	case RPCError, *RPCError,
-		RPCResponse, *RPCResponse:
-		data, err := json.Marshal(m)
-		if err != nil {
-			return err
-		}
-		_, err = w.Write(data)
+func write(w http.ResponseWriter, msg *RPCResponse) error {
+	data, err := json.Marshal(msg)
+	if err != nil {
 		return err
-	default:
-		return fmt.Errorf("invalid RPC structure: %T", msg)
 	}
+	_, err = w.Write(data)
+	return err
 }
 
-func WriteRouterError(w http.ResponseWriter, status int, errm *RPCError) error {
-	return write(w, status, errm)
+func WriteError(w http.ResponseWriter, errm *RPCResponse) error {
+	return write(w, errm)
 }
 
 func WriteResponse(w http.ResponseWriter, response *RPCResponse) error {
-	return write(w, http.StatusOK, response)
+	return write(w, response)
 }
