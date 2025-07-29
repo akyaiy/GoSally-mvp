c62710a7d08762ce02bc7c70c3670a26571dbf6e
Alexey <alex@oblat.lv>
2025-07-29 09:43:27 +0300

    remove some garbage

--- internal/server/sv1/utils.go
+++ internal/server/sv1/utils.go
@@ -5,29 +5,6 @@ import (
 	"os"
 )
 
-// func (h *HandlerV1) errNotFound(w http.ResponseWriter, r *http.Request) {
-// 	utils.WriteJSONError(h.w, http.StatusBadRequest, "invalid request")
-// 	h.log.Error("HTTP request error",
-// 		slog.String("remote", h.r.RemoteAddr),
-// 		slog.String("method", h.r.Method),
-// 		slog.String("url", h.r.URL.String()),
-// 		slog.Int("status", http.StatusBadRequest))
-// }
-
-// func (h *HandlerV1) extractDescriptionStatic(path string) (string, error) {
-// 	data, err := os.ReadFile(path)
-// 	if err != nil {
-// 		return "", err
-// 	}
-
-// 	re := regexp.MustCompile(`---\s*#description\s*=\s*"([^"]+)"`)
-// 	m := re.FindStringSubmatch(string(data))
-// 	if len(m) <= 0 {
-// 		return "", nil
-// 	}
-// 	return m[1], nil
-// }
-
 func (h *HandlerV1) comMatch(ver string, comName string) string {
 	files, err := os.ReadDir(h.cfg.ComDir)
 	if err != nil {
