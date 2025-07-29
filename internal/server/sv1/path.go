b103736a9d4e4fe87bb574ae1cf516e5ce53d610
Alexey <alex@oblat.lv>
2025-07-29 16:41:45 +0300

    add resolveMethodPath to resolve giving methods

---
+++ internal/server/sv1/path.go
@@ -0,0 +1,26 @@
+package sv1
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/akyaiy/GoSally-mvp/internal/server/rpc"
+)
+
+func (h *HandlerV1) resolveMethodPath(method string) (string, error) {
+	if !h.allowedCmd.MatchString(method) {
+		return "", errors.New(rpc.ErrInvalidMethodFormatS)
+	}
+
+	parts := strings.Split(method, ">")
+	relPath := filepath.Join(parts...) + ".lua"
+	fullPath := filepath.Join(h.cfg.ComDir, relPath)
+
+	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+		return "", errors.New(rpc.ErrMethodNotFoundS)
+	}
+
+	return fullPath, nil
+}
