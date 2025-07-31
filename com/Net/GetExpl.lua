148ca535384f83c7541b09d52c94785c02170fd2
Alexey <alex@oblat.lv>
2025-07-31 20:46:02 +0300

    update Net.Http.Get example

--- com/Net/GetExpl.lua
+++ com/Net/GetExpl.lua
@@ -1,4 +1,16 @@
-local resp = Net.Http.Get(true, "https://google.com")
+local reqAddr
+
+if In.Params and In.Params.url then
+  reqAddr = In.Params.url
+else
+  Out.Error = {
+    code = -32602,
+    message = "no url provided"
+  }
+  return
+end
+
+local resp = Net.Http.Get(true, reqAddr)
 if resp then
   Out.Result.answer = {
     status = resp.status,
